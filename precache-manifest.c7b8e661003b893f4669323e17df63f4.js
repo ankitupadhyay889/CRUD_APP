e2863c8be0c4a3181876976a29cf602825ffd377
Ankit <ankitupa7799@gmail.com>
2021-04-15 18:32:50 +0530

    Updates

---
+++ precache-manifest.c7b8e661003b893f4669323e17df63f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0118e9286621b0180234337aceacf92",
+    "url": "/CRUD_APP/index.html"
+  },
+  {
+    "revision": "f42957cff44a7a7f68ad",
+    "url": "/CRUD_APP/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "9d1d82bb76dcbc598638",
+    "url": "/CRUD_APP/static/css/main.bbd96fac.chunk.css"
+  },
+  {
+    "revision": "f42957cff44a7a7f68ad",
+    "url": "/CRUD_APP/static/js/2.40083c41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CRUD_APP/static/js/2.40083c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d1d82bb76dcbc598638",
+    "url": "/CRUD_APP/static/js/main.7db77df6.chunk.js"
+  },
+  {
+    "revision": "5e3a32311527251ef589",
+    "url": "/CRUD_APP/static/js/runtime-main.66062277.js"
+  }
+]);
